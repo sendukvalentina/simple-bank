5e66c6df8f2533e33153fb81487991c174cb8a3d
sendukvalentina <sendukvalentina@gmail.com>
2020-11-14 23:34:55 +0800

    commit all

---
+++ build/precache-manifest.a9f8375feaaf57ff3e57b2518a123b66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a8a517970797cd466c6836ad3a75f34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3df90a425effa5d95c1d",
+    "url": "/static/css/2.8ea44cb0.chunk.css"
+  },
+  {
+    "revision": "8c66bcb0d984ed132076",
+    "url": "/static/css/main.42520db6.chunk.css"
+  },
+  {
+    "revision": "3df90a425effa5d95c1d",
+    "url": "/static/js/2.9af5dea7.chunk.js"
+  },
+  {
+    "revision": "2247c066ec11a016c5ce7a9b0b69c66b",
+    "url": "/static/js/2.9af5dea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c66bcb0d984ed132076",
+    "url": "/static/js/main.7b8a1728.chunk.js"
+  },
+  {
+    "revision": "2d357e18ed83b09c9e0f",
+    "url": "/static/js/runtime-main.bbd4f777.js"
+  },
+  {
+    "revision": "9db70747fd69ee562e61de368bc8490e",
+    "url": "/static/media/login.9db70747.svg"
+  }
+]);
